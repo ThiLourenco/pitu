9814a7f5f9833275203a74905ce486d2f89a01cb
Thiago-l <thigo.l17@yahoo.com>
2020-11-24 03:24:09 -0300

    added dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import linksRouter from './routes/routes';
 import helmet from 'helmet';
+import cors from 'cors';
 
 
 const app = express();
+app.use(cors())
 app.use(helmet());
 app.use(express.json());
 app.use(linksRouter);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 import database from './database';
 
 //database.sync({force: true});
-console.log('Database running at 3306');
+console.log('Database running on 3306');
 
-app.listen(3000, () => console.log('Server running at: 3000'));
+app.listen(3000, () => console.log('Server running on port: 3000'));
