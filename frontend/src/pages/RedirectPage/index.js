5df98f391ae76cde360d71c403a548904231f798
Thiago-l <thigo.l17@yahoo.com>
2020-11-25 05:25:58 -0300

    done

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../../component/Header';
+import Header from '../../components/Header';
 import { Container } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { StatsContainer } from './styles';
