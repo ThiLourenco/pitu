4a85ff2afd6e7a6d43011a6dafe3e565227e0d80
Thiago-l <thigo.l17@yahoo.com>
2020-11-27 18:20:22 -0300

    done

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import app from './app';
 import database from './database';
 
 //database.sync({force: true});
+database.sync();
 console.log('Database running on 3306');
 
 app.listen(3001, () => console.log('Server running on port: 3001'));
